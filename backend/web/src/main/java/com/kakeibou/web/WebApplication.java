cf558cbaed5b3a9e35b7de460ffbc9cc79baa4c2
吉田賢二 <jitianxianer018@gmail.com>
2025-08-18 19:39:37 +0900

    土台を作成

--- backend/web/src/main/java/com/kakeibou/web/WebApplication.java
+++ backend/web/src/main/java/com/kakeibou/web/WebApplication.java
@@ -3,11 +3,14 @@ package com.kakeibou.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Webアプリケーションのエントリポイントです。
+ */
 @SpringBootApplication
 public class WebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
 
 }
