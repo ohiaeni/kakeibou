c6453e8bc8cd25a0b6534903174a27683cafb847
吉田賢二 <jitianxianer018@gmail.com>
2025-08-30 12:08:03 +0900

    feat: ダークモードの切り替え機能を追加
    - app.vue: ダークモードの状態を管理するためのcomputedプロパティを追加し、v-switchを用いてUIに切り替え機能を実装
    - index.vue: メインページの雛形を作成

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,46 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+import { computed } from 'vue'
+
+const theme = useTheme()
+
+// ダークモードの状態を管理
+const isDarkMode = computed({
+  get: () => theme.global.name.value === 'dark',
+  set: (value: boolean) => {
+    theme.global.name.value = value ? 'dark' : 'light'
+  },
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-app-bar :elevation="1">
+            <template #prepend>
+              <v-app-bar-nav-icon />
+            </template>
+            <v-app-bar-title>Application Bar</v-app-bar-title>
+
+            <v-spacer />
+
+            <v-switch
+              v-model="isDarkMode"
+              :prepend-icon="'mdi-white-balance-sunny'"
+              :append-icon="'mdi-weather-night'"
+              color="primary"
+              hide-details
+              inset
+            />
+          </v-app-bar>
+        </v-container>
+
+        <v-container>
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-app>
+  </NuxtLayout>
 </template>
