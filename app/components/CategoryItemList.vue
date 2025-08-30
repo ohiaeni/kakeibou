5103d9f5b59203638b117a526233147b4348120e
吉田賢二 <jitianxianer018@gmail.com>
2025-08-30 14:50:50 +0900

    feat: カテゴリーデータの管理をuseCategoriesコンポーザブルに移動
    - CategoryItemList.vueからサンプルデータを削除し、useCategoriesをインポートして使用するように変更
    - useCategories.tsを新規作成し、カテゴリーデータと関連するメソッドを定義

--- app/components/CategoryItemList.vue
+++ app/components/CategoryItemList.vue
@@ -1,70 +1,7 @@
 <script setup lang="ts">
-// 家計簿カテゴリのサンプルデータ
-const categories = [
-  {
-    id: 1,
-    name: '食費',
-    icon: 'mdi-food',
-    percentage: 65,
-    color: 'primary',
-  },
-  {
-    id: 2,
-    name: '交通費',
-    icon: 'mdi-train',
-    percentage: 30,
-    color: 'primary',
-  },
-  {
-    id: 3,
-    name: '娯楽',
-    icon: 'mdi-movie',
-    percentage: 85,
-    color: 'primary',
-  },
-  {
-    id: 4,
-    name: '光熱費',
-    icon: 'mdi-lightning-bolt',
-    percentage: 45,
-    color: 'primary',
-  },
-  {
-    id: 5,
-    name: '通信費',
-    icon: 'mdi-cellphone',
-    percentage: 20,
-    color: 'primary',
-  },
-  {
-    id: 6,
-    name: '医療費',
-    icon: 'mdi-medical-bag',
-    percentage: 15,
-    color: 'primary',
-  },
-  {
-    id: 7,
-    name: '衣類',
-    icon: 'mdi-tshirt-crew',
-    percentage: 40,
-    color: 'primary',
-  },
-  {
-    id: 8,
-    name: '美容',
-    icon: 'mdi-face-woman',
-    percentage: 25,
-    color: 'primary',
-  },
-  {
-    id: 9,
-    name: 'その他',
-    icon: 'mdi-dots-horizontal',
-    percentage: 10,
-    color: 'primary',
-  },
-]
+import { useCategories } from '~/composables/useCategories'
+
+const { categories } = useCategories()
 </script>
 
 <template>
