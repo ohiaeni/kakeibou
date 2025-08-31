e90e8b5e11b631447929c13c8bafab9200173979
吉田賢二 <jitianxianer018@gmail.com>
2025-08-31 10:53:41 +0900

    feat: ナビゲーションバーをデフォルトレイアウトに統合
    - NavigationBarコンポーネントを削除し、デフォルトレイアウトにナビゲーションバーの機能を直接追加しました。

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
   <v-app>
     <v-main>
-      <NavigationBar />
+      <v-container>
+        <v-app-bar :elevation="1">
+          <div class="w-100 w-md-auto d-flex justify-center justify-md-start align-center h-100 ml-md-4">
+            <NuxtLink to="/" style="text-decoration: none; color: inherit;">
+              <img
+                src="~/assets/img/logo.png"
+                alt="Logo"
+                class="d-block"
+                style="height: 50px;"
+              >
+            </NuxtLink>
+          </div>
+          <v-spacer />
+        </v-app-bar>
+      </v-container>
       <slot />
     </v-main>
   </v-app>
